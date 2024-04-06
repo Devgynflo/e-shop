b689d329539b6c58567b02b3eb2b62e0488afdea
gynflo <ginet.florian.dev@gmail.com>
2024-04-06 17:00:20 +0200

    Feat: Cart UI

---
+++ app/(store)/cart/_components/cart-client.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Heading } from "@/app/components/heading";
+import { Button } from "@/app/components/products/details/_components/button";
+import { useCart } from "@/hooks/use-cart";
+import { NextPage } from "next";
+import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
+
+interface CartClientProps {}
+
+export const CartClient: NextPage<CartClientProps> = ({}) => {
+  const { cartProducts } = useCart();
+
+  if (!cartProducts || cartProducts.length === 0) {
+    return (
+      <section className="flex flex-col items-center">
+        <div className="text-2xl">Your cart is empty</div>
+        <div>
+          <Link
+            href="/"
+            className="mt-2 flex items-center gap-2 text-slate-500"
+          >
+            <MdArrowBack size={20} />
+            <span>Start Shopping</span>
+          </Link>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <Heading title="Shopping Cart" center />
+      <div className="mt-8 grid grid-cols-5 items-center gap-4 pb-2 text-xs">
+        <div className="col-span-2 justify-self-start uppercase">Product</div>
+        <div className="justify-self-center uppercase">Price</div>
+        <div className="justify-self-center uppercase">Quantity</div>
+        <div className="justify-self-end uppercase">Total</div>
+      </div>
+      <div>
+        {cartProducts.map((product) => (
+          <div key={product.id}>{product.name}</div>
+        ))}
+      </div>
+      <div className="flex justify-between gap-4 border-t-[1.5px] border-slate-200 py-4">
+        <div className="w-[100px]">
+          <Button label="Clear Cart" onclick={() => {}} small outline />
+        </div>
+        <div className="flex flex-col items-start gap-1 text-sm">
+          <div className="flex w-full justify-between text-base font-semibold">
+            <span>Subtotal</span>
+            <span>1000€</span>
+          </div>
+          <p className="text-slate-500">
+            Taxes and shipping calculate at checkout
+          </p>
+          <Button onclick={() => {}} label="Checkout" />
+          <Link
+            href="/"
+            className="mt-2 flex items-center gap-2 text-slate-500"
+          >
+            <MdArrowBack size={20} />
+            <span>Continue Shopping</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
--- app/(store)/cart/page.tsx
+++ app/(store)/cart/page.tsx
@@ -1,9 +1,17 @@
+import { Container } from "@/app/components/container";
 import { NextPage } from "next";
+import { CartClient } from "./_components/cart-client";
 
 interface Props {}
 
 const CartPage: NextPage<Props> = ({}) => {
-  return <div>Cart Page</div>;
+  return (
+    <div className="pt-8">
+      <Container>
+        <CartClient />
+      </Container>
+    </div>
+  );
 };
 
 export default CartPage;
